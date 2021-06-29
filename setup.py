542cfd4a039b40eef3f7377bb1b1356319bcad85
konrad <lorenz>
2021-06-28 20:07:58 -0700

    add description type

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from pathlib import Path
 
 setup(
     name="yaghm",
-    version="0.1.1",
+    version="0.1.2",
     description="Minimal git hook manager for the command line.",
     long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
     url="https://www.github.com/metov/yaghm",
     author="Azat Akhmetov",
     author_email="azatinfo@yandex.com",
