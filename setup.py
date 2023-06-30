792fd62f156a8dd2f537eb0fb0991bce0b906b8c
Azat Akhmetov <51468504+metov@users.noreply.github.com>
2023-06-30 17:19:45 -0600

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name="yaghm",
-    version="0.3.0",
+    version="0.3.1",
     description="Minimal git hook manager for the command line.",
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
